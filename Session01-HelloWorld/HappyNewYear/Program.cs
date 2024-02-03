3c91265345f5a106e60a579f48df92a6ea5bd129
dont-wait <nguyentansang15042005@gmail.com>
2024-02-03 11:59:39 +0700

    Ghi chu va chuong trinh dau tien dung ngon ngu C#

---
+++ Session01-HelloWorld/HappyNewYear/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HappyNewYear;	//~ package happynewyear; bên java
+
+class Program
+{
+    static void Main(string[] args) //y chang main() Java và C
+    {
+        Console.WriteLine("Cha`o na(m mo*'i 2024 - Gia'p Thi`n");	//System.out.println("") của bên Java
+    	  
+        Console.WriteLine("\nPress any key to exit...");
+    	  
+        Console.ReadLine(); //chờ ai đó gõ phím thì mới kết thúc app
+    }
+}
