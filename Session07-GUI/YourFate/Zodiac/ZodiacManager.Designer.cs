be053123a4c17949e199a8d185b1179e3a62e255
dont-wait <nguyentansang15042005@gmail.com>
2024-03-11 02:53:54 +0700

    add fate program

---
+++ Session07-GUI/YourFate/Zodiac/ZodiacManager.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Zodiac
+{
+    partial class ZodiacManager
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblWelcome = new Label();
+            btnShowImage = new Button();
+            picImage = new PictureBox();
+            pnlImage = new Panel();
+            btnQuit = new Button();
+            btnCheckZodiac = new Button();
+            lblDay = new Label();
+            lblMonth = new Label();
+            lblYourZodiac = new Label();
+            txbBirthDay = new TextBox();
+            txbMonth = new TextBox();
+            lblNoCopyright = new Button();
+            ((System.ComponentModel.ISupportInitialize)picImage).BeginInit();
+            pnlImage.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblWelcome
+            // 
+            lblWelcome.AutoSize = true;
+            lblWelcome.BackColor = Color.Red;
+            lblWelcome.Font = new Font("Segoe UI", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblWelcome.ForeColor = Color.Yellow;
+            lblWelcome.Location = new Point(-1, 0);
+            lblWelcome.Name = "lblWelcome";
+            lblWelcome.Size = new Size(559, 86);
+            lblWelcome.TabIndex = 0;
+            lblWelcome.Text = "Zodiac Calculator";
+            // 
+            // btnShowImage
+            // 
+            btnShowImage.FlatStyle = FlatStyle.Flat;
+            btnShowImage.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnShowImage.Location = new Point(162, 316);
+            btnShowImage.Name = "btnShowImage";
+            btnShowImage.Size = new Size(154, 34);
+            btnShowImage.TabIndex = 3;
+            btnShowImage.Text = "show Image";
+            btnShowImage.UseVisualStyleBackColor = true;
+            btnShowImage.Click += btnShowImage_Click;
+            // 
+            // picImage
+            // 
+            picImage.BackColor = Color.WhiteSmoke;
+            picImage.Location = new Point(13, 15);
+            picImage.Name = "picImage";
+            picImage.Size = new Size(336, 346);
+            picImage.SizeMode = PictureBoxSizeMode.AutoSize;
+            picImage.TabIndex = 3;
+            picImage.TabStop = false;
+            picImage.Click += pictureBox2_Click;
+            // 
+            // pnlImage
+            // 
+            pnlImage.AutoScroll = true;
+            pnlImage.BackColor = Color.FromArgb(255, 128, 255);
+            pnlImage.Controls.Add(picImage);
+            pnlImage.Location = new Point(495, 89);
+            pnlImage.Name = "pnlImage";
+            pnlImage.Size = new Size(366, 383);
+            pnlImage.TabIndex = 4;
+            pnlImage.Paint += pnlImage_Paint;
+            // 
+            // btnQuit
+            // 
+            btnQuit.FlatStyle = FlatStyle.Flat;
+            btnQuit.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnQuit.Location = new Point(322, 316);
+            btnQuit.Name = "btnQuit";
+            btnQuit.Size = new Size(154, 34);
+            btnQuit.TabIndex = 4;
+            btnQuit.Text = "Quit";
+            btnQuit.UseVisualStyleBackColor = true;
+            btnQuit.Click += btnQuit_Click;
+            // 
+            // btnCheckZodiac
+            // 
+            btnCheckZodiac.FlatStyle = FlatStyle.Flat;
+            btnCheckZodiac.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCheckZodiac.Location = new Point(2, 316);
+            btnCheckZodiac.Name = "btnCheckZodiac";
+            btnCheckZodiac.Size = new Size(154, 34);
+            btnCheckZodiac.TabIndex = 2;
+            btnCheckZodiac.Text = "Check Zodiac";
+            btnCheckZodiac.UseVisualStyleBackColor = true;
+            btnCheckZodiac.Click += btnCheckZodiac_Click;
+            // 
+            // lblDay
+            // 
+            lblDay.AutoSize = true;
+            lblDay.BackColor = Color.Red;
+            lblDay.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblDay.ForeColor = Color.Yellow;
+            lblDay.Location = new Point(26, 140);
+            lblDay.Name = "lblDay";
+            lblDay.Size = new Size(114, 21);
+            lblDay.TabIndex = 7;
+            lblDay.Text = "Your birthday";
+            // 
+            // lblMonth
+            // 
+            lblMonth.AutoSize = true;
+            lblMonth.BackColor = Color.Red;
+            lblMonth.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblMonth.ForeColor = Color.Yellow;
+            lblMonth.Location = new Point(26, 187);
+            lblMonth.Name = "lblMonth";
+            lblMonth.Size = new Size(141, 21);
+            lblMonth.TabIndex = 8;
+            lblMonth.Text = "Your birth month";
+            // 
+            // lblYourZodiac
+            // 
+            lblYourZodiac.AutoSize = true;
+            lblYourZodiac.BackColor = Color.Red;
+            lblYourZodiac.Font = new Font("Segoe UI", 24F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblYourZodiac.ForeColor = Color.Yellow;
+            lblYourZodiac.Location = new Point(2, 456);
+            lblYourZodiac.Name = "lblYourZodiac";
+            lblYourZodiac.Size = new Size(302, 45);
+            lblYourZodiac.TabIndex = 5;
+            lblYourZodiac.Text = "Your Zodiac sign is";
+            // 
+            // txbBirthDay
+            // 
+            txbBirthDay.Location = new Point(194, 140);
+            txbBirthDay.Name = "txbBirthDay";
+            txbBirthDay.Size = new Size(100, 23);
+            txbBirthDay.TabIndex = 0;
+            // 
+            // txbMonth
+            // 
+            txbMonth.Location = new Point(194, 189);
+            txbMonth.Name = "txbMonth";
+            txbMonth.Size = new Size(100, 23);
+            txbMonth.TabIndex = 1;
+            // 
+            // lblNoCopyright
+            // 
+            lblNoCopyright.FlatStyle = FlatStyle.Flat;
+            lblNoCopyright.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblNoCopyright.Location = new Point(707, 478);
+            lblNoCopyright.Name = "lblNoCopyright";
+            lblNoCopyright.Size = new Size(154, 34);
+            lblNoCopyright.TabIndex = 9;
+            lblNoCopyright.Text = "© 2024 - Sang.NT";
+            lblNoCopyright.UseVisualStyleBackColor = true;
+            // 
+            // ZodiacManager
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Red;
+            ClientSize = new Size(873, 541);
+            Controls.Add(lblNoCopyright);
+            Controls.Add(txbMonth);
+            Controls.Add(txbBirthDay);
+            Controls.Add(lblYourZodiac);
+            Controls.Add(lblMonth);
+            Controls.Add(lblDay);
+            Controls.Add(btnCheckZodiac);
+            Controls.Add(btnQuit);
+            Controls.Add(pnlImage);
+            Controls.Add(btnShowImage);
+            Controls.Add(lblWelcome);
+            ForeColor = Color.Yellow;
+            Name = "ZodiacManager";
+            Text = "Welcome to Zodiac calculator";
+            Load += ZodiacManager_Load;
+            ((System.ComponentModel.ISupportInitialize)picImage).EndInit();
+            pnlImage.ResumeLayout(false);
+            pnlImage.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblWelcome;
+        private Button btnShowImage;
+        private PictureBox picImage;
+        private Panel pnlImage;
+        private Button btnQuit;
+        private Button btnCheckZodiac;
+        private Label lblDay;
+        private Label lblMonth;
+        private Label lblYourZodiac;
+        private TextBox txbBirthDay;
+        private TextBox txbMonth;
+        private Button lblNoCopyright;
+    }
+}
---
+++ Session07-GUI/YourFate/Zodiac/ZodiacManager.cs
@@ -0,0 +1,99 @@
+﻿using Services;
+
+namespace Zodiac
+{
+    //Class Form1 chính là class kế thừa (inherit/
+    //inheritance) cái class gốc mà Windows cung cấp để
+    //render ra cửa sổ tương tác
+
+    //Form là class Cha, Parent, class Gốc có khả năng
+    //render 1 HCN ~ web page để cung cấp mặt sàn tương 
+    //tác với user
+
+    //Ta lập trình app Gui, app destop, tức là cái
+    //app chạy trên nền Destop, có icon biểu tượng trên
+    //thanh Taskbar, chính là tạo ra class kế thừa class Form.
+    //                          Con kế thừa Cha
+    //                          Con extend Cha
+    //                          Dog         Pet
+    //                          Rectangle   Shape
+    //chữ kế thừa trong Java là extand
+    //                  C#      :
+    public partial class ZodiacManager : Form
+    {
+        public ZodiacManager()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShowImage_Click(object sender, EventArgs e)
+        {
+            //1. đưa ảnh vào ram
+            //Image img = Image.FromFile(@"signs\hauGai.jpg");
+
+            Image img = Image.FromFile(@"signs\hauGai.jpg");
+
+
+            //Image img = Image.FromFile("signs\\hauGai.jpg");
+
+            //đường dẫn đến bức ảnh thì phải là dấu \ với window
+            //dầu / với Linus, Mac os
+            // dấu \ biểu diễn kí tự đặc biệt \n xuống hàng \t tab
+            //do đó ta phải \\ để cho win biết rằng
+            //ta đang \ theo style bình thường
+
+            //2. Show ảnh <img src="">
+            picImage.Image = img;
+        }
+
+        private void ZodiacManager_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCheckZodiac_Click(object sender, EventArgs e)
+        {
+            //gọi thư viện để trả về cung hoàng đạo
+            //trước đó cần lấy được ngày và tháng sinh đang nằm trong 2 ô Text. Toàn
+            //bộ gõ từ bàn phím là text. Ta phải convert về số. Java: Integer.ParseInt
+            //(string)
+
+            int day = int.Parse(txbBirthDay.Text);   //gọi Text
+            int month = int.Parse(txbMonth.Text);   //gọi Text
+
+            string zodiacEN = ZodiacCalculator.GetZodiacEnglish(month, day);
+            string zodiacVI = ZodiacCalculator.GetZodiacVietnamese(zodiacEN);
+
+            string zodiacImage = @"signs\" + zodiacEN + ".jpg"; //lấy tên cung(file) + jpg convert ra ảnh
+
+            //load ảnh và cập tập cái status
+            Image img = Image.FromFile(zodiacImage);
+            picImage.Image = img;
+
+            lblYourZodiac.Text = "Your zodiac sign: " + zodiacEN + " | " + zodiacVI;
+
+            //TODO: NHỚ VALIDATION NGÀY THÁNG CÓ HỢP LỆ, CÓ NHẬP HAY KHÔNG
+            //IF ( Ô TEXT LẤY TEXT RA RỒI TÍNH
+
+
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pnlImage_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
+
+//private void btnShowImage_Click(object sender, EventArgs e)
+//{}
